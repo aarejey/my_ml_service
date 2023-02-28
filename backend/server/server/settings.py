ab16a8f7df252f844b321052bd086fcfb05f3e0a
aarejey <rejeesh003@gmail.com>
2023-02-28 14:00:15 +0530

    Docker push

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')i1t8@6zawhw2!v+9+ou3y$0vhc5q(bs#zq)3k3mi289yrc$bq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -120,5 +120,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
